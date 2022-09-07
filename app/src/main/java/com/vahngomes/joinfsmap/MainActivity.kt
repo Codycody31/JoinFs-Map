4e7a3bbc37990dd97f43be3f88c5aba60329de69
Codycody31 <Github@codycody.org>
2022-09-07 19:25:03 -0400

    v1

--- app/src/main/java/com/vahngomes/joinfsmap/MainActivity.kt
+++ app/src/main/java/com/vahngomes/joinfsmap/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.vahngomes.joinfsmap
-
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.net.Network
+import android.os.Build
 import android.os.Bundle
-
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 class MainActivity : AppCompatActivity() {
+    private var mWebview: WebView? = null
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createNotificationChannel()
+        startWebView()
+        startnotification()
+    }
+    override fun onDestroy() {
+        // delete notification if it exists
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancel(1)
+        super.onDestroy()
+
+        }
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun startWebView(){
+        mWebview = WebView(this)
+        mWebview!!.settings.javaScriptEnabled = true
+        mWebview!!.loadUrl("https://joinfsmap.dotdash.space/")
+        setContentView(mWebview)
+    }
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("JoinFsMap", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+    private fun startnotification() {
+        val builder = NotificationCompat.Builder(this, "JoinFsMap")
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle("Map Connected")
+            .setContentText("Website ported over!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setOnlyAlertOnce(true)
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(1, builder.build())
+        }
     }
 }
