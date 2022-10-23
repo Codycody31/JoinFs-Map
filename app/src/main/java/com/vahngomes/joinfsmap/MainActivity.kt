df948af63f31349b021b2071d4f8094001c7b5a7
Codycody31 <Github@codycody.org>
2022-10-23 17:05:00 -0400

    Improved some settings!

--- app/src/main/java/com/vahngomes/joinfsmap/MainActivity.kt
+++ app/src/main/java/com/vahngomes/joinfsmap/MainActivity.kt
@@ -1,69 +1,100 @@
+@file:Suppress("DEPRECATION", "unused", "SpellCheckingInspection")
+
 package com.vahngomes.joinfsmap
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
-import android.net.Network
+import android.net.ConnectivityManager
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+
+
+@Suppress("SpellCheckingInspection")
 class MainActivity : AppCompatActivity() {
     private var mWebview: WebView? = null
-    @SuppressLint("SetJavaScriptEnabled")
+    var notificationIdNumerator = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        createNotificationChannel()
         startWebView()
-        startnotification()
     }
     override fun onDestroy() {
         // delete notification if it exists
         val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.cancel(1)
         super.onDestroy()
-
         }
-    @SuppressLint("SetJavaScriptEnabled")
+    @SuppressLint("SetTextI18n", "SetJavaScriptEnabled")
     private fun startWebView(){
-        mWebview = WebView(this)
-        mWebview!!.settings.javaScriptEnabled = true
-        mWebview!!.loadUrl("https://joinfsmap.dotdash.space/")
-        setContentView(mWebview)
-    }
-    private fun createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = getString(R.string.channel_name)
-            val descriptionText = getString(R.string.channel_description)
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel("JoinFsMap", name, importance).apply {
-                description = descriptionText
-            }
-            // Register the channel with the system
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
+        val text: TextView = findViewById(R.id.android_text)
+        val reload: Button = findViewById(R.id.reload_button)
+        if (!isNetworkConnected()){
+            text.text = "Website failed to port over!"
+            text.visibility = View.VISIBLE
+            reload.visibility = View.VISIBLE
+            val mToast = Toast.makeText(applicationContext,"Map Not Connected!", Toast.LENGTH_SHORT)
+            mToast.show()
+        }
+        else {
+            mWebview = WebView(this)
+            mWebview!!.settings.javaScriptEnabled = true
+            mWebview!!.loadUrl("https://joinfsmap.dotdash.space/")
+            setContentView(mWebview)
         }
     }
-    private fun startnotification() {
-        val builder = NotificationCompat.Builder(this, "JoinFsMap")
-            .setSmallIcon(R.drawable.ic_notification)
-            .setContentTitle("Map Connected")
-            .setContentText("Website ported over!")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setAutoCancel(true)
-            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-            .setOnlyAlertOnce(true)
-        with(NotificationManagerCompat.from(this)) {
-            // notificationId is a unique int for each notification that you must define
-            notify(1, builder.build())
+    private fun isNetworkConnected(): Boolean {
+        val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        return cm.activeNetworkInfo != null && cm.activeNetworkInfo!!.isConnected
+    }
+
+    fun reloadPage(@Suppress("UNUSED_PARAMETER") v:View) {
+        if (!isNetworkConnected()){
+            val mToast = Toast.makeText(applicationContext,"Please turn on your internet!", Toast.LENGTH_SHORT)
+            mToast.show()
+        }else {
+            finish()
+            startActivity(intent)
         }
+    }
+}
+
+private fun MainActivity.startnotification(title: String, content: String) {
+    val builder = NotificationCompat.Builder(this, "JoinFsMap")
+        .setSmallIcon(R.drawable.ic_notification)
+        .setContentTitle(title)
+        .setContentText(content)
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        .setAutoCancel(true)
+        .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+        .setOnlyAlertOnce(true)
+    with(NotificationManagerCompat.from(this)) {
+        // notificationId is a unique int for each notification that you must define
+        notify(notificationIdNumerator + 1, builder.build())
+    }
+}
+
+private fun MainActivity.createNotificationChannel() {
+    // Create the NotificationChannel, but only on API 26+ because
+    // the NotificationChannel class is new and not in the support library
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val name = getString(R.string.channel_name)
+        val descriptionText = getString(R.string.channel_description)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel("JoinFsMap", name, importance).apply {
+            description = descriptionText
+        }
+        // Register the channel with the system
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
